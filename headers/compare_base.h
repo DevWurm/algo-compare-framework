6d7aa2fd901a61a023c2cba517c95ad4dc8a4a65
DevWurm <devwurm@gmx.net>
2015-04-16 18:05:32 +0200

    Added License declaration

--- headers/compare_base.h
+++ headers/compare_base.h
@@ -1,9 +1,38 @@
 /*
- * compare_base.h
- *
- *  Created on: 14.04.2015
- *      Author: leo
- */
+	 * License
+
+	 * Copyright 2015 DevWurm
+
+	 * This file is part of compare_library.
+
+	 *  compare_library is free software: you can redistribute it and/or modify
+	    it under the terms of the GNU General Public License as published by
+	    the Free Software Foundation, either version 3 of the License, or
+	    (at your option) any later version.
+
+	    compare_library is distributed in the hope that it will be useful,
+	    but WITHOUT ANY WARRANTY; without even the implied warranty of
+	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	    GNU General Public License for more details.
+
+	    You should have received a copy of the GNU General Public License
+	    along with compare_library.  If not, see <http://www.gnu.org/licenses/>.
+
+	    Diese Datei ist Teil von compare_library.
+
+	    compare_library ist Freie Software: Sie können es unter den Bedingungen
+	    der GNU General Public License, wie von der Free Software Foundation,
+	    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+	    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+	    compare_library wird in der Hoffnung, dass es nützlich sein wird, aber
+	    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+	    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+	    Siehe die GNU General Public License für weitere Details.
+
+	    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+	    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef HEADERS_COMPARE_BASE_H_
 #define HEADERS_COMPARE_BASE_H_
